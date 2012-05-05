9d0437a0d5eadabc8eb14980cb9a37a74c2cc895
smartboyathome <smartboyathome@gmail.com>
2012-05-04 20:14:22 -0700

    The beginnings of text formatting have been added! Bold, italic, and underline formatting work!

--- SmartTE/Widgets/FormattableTextBuffer.py
+++ SmartTE/Widgets/FormattableTextBuffer.py
@@ -1,10 +1,28 @@
-from SmartTE.Widgets import UndoableTextBuffer
+from SmartTE.Widgets.UndoableTextBuffer import TextBuffer
 from SmartTE.Signals import ToolbarSignals, FormatSignals
 from pydispatch import dispatcher
+from gi.repository import Pango, Gtk
 
-class FormattableTextBuffer(UndoableTextBuffer.TextBuffer):
+class FormattableTextBuffer(TextBuffer):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.initializeDefaultTags()
+        self.connectFormattingSignals()
+
+    def initializeDefaultTags(self):
+        self.generateTag('bold', property_args=[('weight', Pango.Weight.BOLD)])
+        self.generateTag('italic', property_args=[('style', Pango.Style.ITALIC)])
+        self.generateTag('underline', property_args=[('underline', Pango.Underline.SINGLE)])
+        self.generateTag('error', property_args=[('underline', Pango.Underline.ERROR)])
+        self.generateTag('ignore')
+
+    def generateTag(self, name, property_args=[]):
+        tag = Gtk.TextTag.new(name)
+        for arg_set in property_args:
+            tag.set_property(*arg_set)
+        self.get_tag_table().add(tag)
+
+    def connectFormattingSignals(self):
         dispatcher.connect(self.onBoldActive, signal=ToolbarSignals.BOLD_ACTIVE, sender=dispatcher.Any)
         dispatcher.connect(self.onBoldInactive, signal=ToolbarSignals.BOLD_INACTIVE, sender=dispatcher.Any)
         dispatcher.connect(self.onItalicActive, signal=ToolbarSignals.ITALIC_ACTIVE, sender=dispatcher.Any)
@@ -13,24 +31,34 @@ class FormattableTextBuffer(UndoableTextBuffer.TextBuffer):
         dispatcher.connect(self.onUnderlineInactive, signal=ToolbarSignals.UNDERLINE_INACTIVE, sender=dispatcher.Any)
         dispatcher.connect(self.onFamilyChange, signal=ToolbarSignals.FAMILY_CHANGE, sender=dispatcher.Any)
         dispatcher.connect(self.onSizeChange, signal=ToolbarSignals.SIZE_CHANGE, sender=dispatcher.Any)
-    
+
+    def activateTag(self, name):
+        if self.get_has_selection():
+            bounds = self.get_selection_bounds()
+            self.apply_tag_by_name(name, bounds[0], bounds[1])
+
+    def deactivateTag(self, name):
+        if self.get_has_selection():
+            bounds = self.get_selection_bounds()
+            self.remove_tag_by_name(name, bounds[0], bounds[1])
+
     def onBoldActive(self):
-        pass
-    
+        self.activateTag('bold')
+
     def onBoldInactive(self):
-        pass
+        self.deactivateTag('bold')
 
     def onItalicActive(self):
-        pass
+        self.activateTag('italic')
 
     def onItalicInactive(self):
-        pass
+        self.deactivateTag('italic')
 
     def onUnderlineActive(self):
-        pass
+        self.activateTag('underline')
 
     def onUnderlineInactive(self):
-        pass
+        self.deactivateTag('underline')
 
     def onFamilyChange(self):
         pass
